0bca077973d031b33d8816812666059001f88975
Nigel Chen <nigelchen2014@gmail.com>
2023-05-12 23:27:13 -0700

    Update

--- cxx_model/main.cpp
+++ cxx_model/main.cpp
@@ -5,22 +5,13 @@ using namespace std;
 
 int main(void) {
     c3_model model;
-    uint64_t s = 12888;
-    uint64_t ptr = model.malloc_c3(s);
+    uint64_t ptr = model.malloc_c3(20);
 
-    if (ptr == MEM_SIZE) {
-        cout << "Error when allocating size " << s << endl;
-        return 0;
-    }
-
-    cout << "Encrypted Address: " << hex << ptr << endl;
-    cout << "WRITING WITHIN BOUNDS" << endl;
+    // write within bounds
     model.store_c3(ptr + 5, 11);
-    cout << "READING OOB" << endl;
-    bool res = model.read_c3(ptr + 9, 11); // read +4 bytes OOB
-    cout << endl;
 
-    // assertion fails
+    // read +4 bytes OOB
+    bool res = model.read_c3(ptr + 9, 11); 
     assert (res);
 
     return 0;
